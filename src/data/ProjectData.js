9d75b427a3d4f067a183b46b6f84909e6a4fbdec
Elvin <elvinsrkrov@gmail.com>
2024-06-21 19:17:53 +0400

    initial folder structure and header, hero sections added

---
+++ src/data/ProjectData.js
@@ -0,0 +1,137 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/src/assets/images/CineFlix Preview.png",
+    title: "CineFlix",
+    description:
+      "A cross-platform mobile game (in closed testing) where players try to find the missing pieces of a story.",
+    tech_stack: ["JavaScript", "Sass", "Html"],
+    github_url: "https://github.com/ElvinWeb/CineFlix.git",
+    demo_url: "https://cine-flix-site.vercel.app/",
+  },
+  {
+    img: "/src/assets/images/Forkify Preview.png",
+    title: "Forkify",
+    description:
+      "A safety application for the Renewable Resources Dept. of BCIT",
+    tech_stack: ["JavaScript", "Sass", "Html", "Parcel"],
+    github_url: "https://github.com/ElvinWeb/Forkify.git",
+    demo_url: "https://forkify-site.vercel.app/",
+  },
+  {
+    img: "/src/assets/images/GitFinder preview.png",
+    title: "GitFinder",
+    description:
+      "An app where users can purchase online therapy sessions and chat with their therapists.",
+    tech_stack: ["JavaScript", "Css", "Html", "Parcel"],
+    github_url: "https://github.com/ElvinWeb/GitFinder.git",
+    demo_url: "https://git-finder-site.vercel.app/",
+  },
+  {
+    img: "/src/assets/images/Rainery Preview.png",
+    title: "Rainery",
+    description:
+      "A pokedex app that displays pokemon data, plays OSTs and makes pokemons dance.",
+    tech_stack: ["JavaScript", "Sass", "Html", "Parcel"],
+    github_url: "https://github.com/ElvinWeb/Rainery.git",
+    demo_url: "https://rainery-app.vercel.app/",
+  },
+  {
+    img: "/src/assets/images/Forkify Preview.png",
+    title: "rmtDev",
+    description:
+      "An app to detect if a job posting is a scam or not using its description.",
+    tech_stack: ["JavaScript", "Css", "Html", "Parcel"],
+    github_url: "https://github.com/ElvinWeb/rmtDev.git",
+    demo_url: "https://rmt-dev-site.vercel.app/",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+    name: "NextJs",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    name: "Typescript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "React Native",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+    name: "Firebase",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original.svg",
+    name: "Flask",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+    name: "Java",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+    name: "Material UI",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+    name: "Figma",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg",
+    name: "TailwindCSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
